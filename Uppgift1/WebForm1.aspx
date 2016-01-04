63f8eec2dd6072de92c45050e20d8f1d8008ded0
kriller83 <krirag12@student.hh.se>
2016-01-04 15:28:11 +0100

    uppgift1

--- Uppgift1/WebForm1.aspx
+++ Uppgift1/WebForm1.aspx
@@ -9,8 +9,19 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        Skriv in antal liter:
+        <asp:TextBox ID="LiterBox" runat="server"></asp:TextBox><br/>
+        Konvertera till dl, cl eller ml. <br/>
+        <asp:Button ID="DeciliterButton" runat="server" Text="Deciliter" OnClick="DeciliterButton_Click" />
+        <asp:Button ID="CentiliterButton" runat="server" Text="Centiliter" OnClick="CentiliterButton_Click" />
+        <asp:Button ID="MilliliterButton" runat="server" Text="Milliliter" OnClick="MilliliterButton_Click" />
+        <br/>
+        <asp:Label ID="ResultatLabel" runat="server" Text="Resultat"></asp:Label>
     </div>
+        <asp:Button ID="Button1" runat="server" Text="Alla resultat" OnClick="Button1_Click" /><br/>
+        Deciliter: <asp:Label ID="Label1" runat="server" Text="Deciliter"></asp:Label><br/>
+        Centiliter: <asp:Label ID="Label2" runat="server" Text="Centiliter"></asp:Label><br/>
+        Milliliter: <asp:Label ID="Label3" runat="server" Text="Milliliter"></asp:Label><br/>
     </form>
 </body>
 </html>
--- Uppgift1/WebForm1.aspx.cs
+++ Uppgift1/WebForm1.aspx.cs
@@ -9,8 +9,55 @@ namespace Uppgift1
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        public double tal1;
+        public double result;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+
+        }
+
+        protected void DeciliterButton_Click(object sender, EventArgs e)
+        {
+            //Översätter string till double, om det går (try.Parse)
+            double.TryParse(LiterBox.Text, out tal1);
+            result = (tal1*10);
+            ResultatLabel.Text = result.ToString();
+
+        }
+
+        protected void CentiliterButton_Click(object sender, EventArgs e)
+        {
+            double.TryParse(LiterBox.Text, out tal1);
+            result = (tal1*100);
+            ResultatLabel.Text = result.ToString();
+
+        }
+
+        protected void MilliliterButton_Click(object sender, EventArgs e)
+        {
+            double.TryParse(LiterBox.Text, out tal1);
+            result = (tal1*1000);
+            ResultatLabel.Text = result.ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double result1;
+            double result2;
+            double result3;
+            double.TryParse(LiterBox.Text, out tal1);
+            result1 = (tal1*10);
+            Label1.Text = result1.ToString();
+
+            double.TryParse(LiterBox.Text, out tal1);
+            result2 = (tal1*100);
+            Label2.Text = result2.ToString();
+
+            double.TryParse(LiterBox.Text, out tal1);
+            result3 = (tal1*1000);
+            Label3.Text = result3.ToString();
 
         }
     }
